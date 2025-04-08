77bca34347fcff3094d6fd8972c29dcffae1c471
Tami Takada <tamitakada@gmail.com>
2025-04-08 10:08:22 -0400

    last 3 tests

--- src/test/java/org/apache/commons/lang3/time/FastDateParserTest.java
+++ src/test/java/org/apache/commons/lang3/time/FastDateParserTest.java
@@ -668,38 +668,38 @@ public class FastDateParserTest extends AbstractLangTest {
     //     testSdfAndFdp(dpProvider, "yyyy-MM-dd 'QED'", "2003-02-10 qed", true);
     // }
 
-    // @Test
-    // public void testTimeZoneMatches() {
-    //     final DateParser parser = getInstance(yMdHmsSZ, REYKJAVIK);
-    //     assertEquals(REYKJAVIK, parser.getTimeZone());
-    // }
+    @Test
+    public void testTimeZoneMatches() {
+        final DateParser parser = getInstance(yMdHmsSZ, REYKJAVIK);
+        assertEquals(REYKJAVIK, parser.getTimeZone());
+    }
 
-    // @Test
-    // public void testToStringContainsName() {
-    //     final DateParser parser = getInstance(YMD_SLASH);
-    //     assertTrue(parser.toString().startsWith("FastDate"));
-    // }
+    @Test
+    public void testToStringContainsName() {
+        final DateParser parser = getInstance(YMD_SLASH);
+        assertTrue(parser.toString().startsWith("FastDate"));
+    }
 
     // we cannot use historic dates to test time zone parsing, some time zones have second offsets
     // as well as hours and minutes which makes the z formats a low fidelity round trip
     @Test
     public void testTzParses() throws Exception {
         // Check that all Locales can parse the time formats we use
-        // for (final Locale locale : Locale.getAvailableLocales()) {
-        //     final FastDateParser fdp = new FastDateParser("yyyy/MM/dd z", TimeZone.getDefault(), locale);
+        for (final Locale locale : Locale.getAvailableLocales()) {
+            final FastDateParser fdp = new FastDateParser("yyyy/MM/dd z", TimeZone.getDefault(), locale);
 
-        //     for (final TimeZone timeZone : new TimeZone[] {NEW_YORK, REYKJAVIK, TimeZones.GMT}) {
-        //         final Calendar cal = Calendar.getInstance(timeZone, locale);
-        //         cal.clear();
-        //         cal.set(Calendar.YEAR, 2000);
-        //         cal.set(Calendar.MONTH, 1);
-        //         cal.set(Calendar.DAY_OF_MONTH, 10);
-        //         final Date expected = cal.getTime();
+            for (final TimeZone timeZone : new TimeZone[] {NEW_YORK, REYKJAVIK, TimeZones.GMT}) {
+                final Calendar cal = Calendar.getInstance(timeZone, locale);
+                cal.clear();
+                cal.set(Calendar.YEAR, 2000);
+                cal.set(Calendar.MONTH, 1);
+                cal.set(Calendar.DAY_OF_MONTH, 10);
+                final Date expected = cal.getTime();
 
-        //         final Date actual = fdp.parse("2000/02/10 " + timeZone.getDisplayName(locale));
-        //         assertEquals(expected, actual, "timeZone:" + timeZone.getID() + " locale:" + locale.getDisplayName());
-        //     }
-        // }
+                final Date actual = fdp.parse("2000/02/10 " + timeZone.getDisplayName(locale));
+                assertEquals(expected, actual, "timeZone:" + timeZone.getID() + " locale:" + locale.getDisplayName());
+            }
+        }
     }
 
     private void validateSdfFormatFdpParseEquality(final String formatStr, final Locale locale, final TimeZone timeZone,
